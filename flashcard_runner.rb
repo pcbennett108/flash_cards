bd97802facee1a6ebc9dc7c084c83e07778ea9f7
pcbennett108 <pcbennett42@gmail.com>
2023-05-10 20:45:31 -0600

    Complete runner file without START method for iteration 3

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -7,13 +7,34 @@ require './lib/round'
 card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
 card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+card_4 = Card.new("What is the highest mountain in North America?", "Denali", :Geography)
 
-cards = [card_1, card_2, card_3]
+cards = [card_1, card_2, card_3, card_4]
 deck = Deck.new(cards)
 round = Round.new(deck)
-turn_1 = round.take_turn("Juneau")
-turn_2 = round.take_turn("Venus")
+# turn_1 = round.take_turn("Juneau")
+# turn_2 = round.take_turn("Venus")
 
 
-require "pry" ; binding.pry
+#require "pry" ; binding.pry
 
+
+tries = 0
+puts "Welcome! You're playing with 4 cards."
+puts "-------------------------------------------------"
+
+while tries < deck.count
+  puts "This is card number #{round.card_count + 1} out of #{deck.count}."
+  puts "Question: #{round.current_card.question}"
+  guess = gets.chomp
+  turn = round.take_turn(guess)
+  #? why would it not work without the var assignment - wrong num of args
+  p turn.feedback
+  puts ""
+  tries += 1
+end
+
+puts "****** Game over! ******"
+puts "You had #{round.number_correct} correct guesses out of #{deck.count} for a total score of #{round.percent_correct}%"
+puts "STEM - #{round.percent_correct_by_category(:STEM)}% correct"
+puts "Geography - #{round.percent_correct_by_category(:Geography)}% correct"
--- lib/round.rb
+++ lib/round.rb
@@ -4,43 +4,43 @@ require './lib/turn'
 require './lib/deck'
 
 class Round
-  attr_reader :deck, :turns, :number_correct, :card_count
-  def initialize(deck)
-    @deck = deck
-    @turns = []
-    @number_correct = 0
-    @card_count = 0
-  end 
+    attr_reader :deck, :turns, :number_correct, :card_count
+    def initialize(deck)
+      @deck = deck
+      @turns = []
+      @number_correct = 0
+      @card_count = 0
+    end 
 
-  def current_card
-    deck.cards[card_count]
-  end
-  
-  def take_turn(guess)
-    current_turn = Turn.new(guess, current_card)
-    @turns << current_turn
-    @number_correct += 1 if current_turn.correct?
-    @card_count += 1
-    current_turn
-  end
-
-  def number_correct_by_category(category)
-    cat_match = turns.find_all { |turn| turn.card.category == category }
-    number_correct = 0
-    cat_match.each do |match|
-      number_correct += 1 if match.guess == match.card.answer
+    def current_card
+      deck.cards[card_count]
     end
-    number_correct
-  end
+    
+    def take_turn(guess)
+      current_turn = Turn.new(guess, current_card)
+      @turns << current_turn
+      @number_correct += 1 if current_turn.correct?
+      @card_count += 1
+      current_turn
+    end
 
-  def percent_correct
-    number_correct.to_f / card_count.to_f * 100
-  end
+    def number_correct_by_category(category)
+      cat_match = turns.find_all { |turn| turn.card.category == category }
+      number_correct = 0
+      cat_match.each do |match|
+        number_correct += 1 if match.guess == match.card.answer
+      end
+      number_correct
+    end
 
-  def percent_correct_by_category(category)
-    category_total = deck.cards_in_category(category)
-    number_correct_by_category(category).to_f / category_total.length.to_f * 100
-  end
+    def percent_correct
+      number_correct.to_f / card_count.to_f * 100
+    end
+
+    def percent_correct_by_category(category)
+      category_total = deck.cards_in_category(category)
+      number_correct_by_category(category).to_f / category_total.length.to_f * 100
+    end
 
 end
 #? suggestions for cleaning up block @ 27
